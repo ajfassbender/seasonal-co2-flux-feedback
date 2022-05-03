990aeaec39baa0618b0fc7a5981e92a93eb878e5
Andrea Fassbender <55896000+ajfassbender@users.noreply.github.com>
2022-05-03 07:47:10 -0700

    Add files via upload

---
+++ Figure_4_amplitude_changes_1950_2090.m
@@ -0,0 +1,181 @@
+%% Figure 4 Calculations
+%
+% DESCRIPTION:
+%   1950s to 2090s ensemble mean pCO2 amplitude changes.
+%
+% USER INPUT:
+%   data_path: directory containing reprocessed .nc files
+%   fig_path:  directory where figure will be saved
+%
+% FUNCTIONS CALLED:
+%   getArea: Computes surface area (m2) of each 1° latitide by 1° longitude horizontal grid
+% 
+% AUTHOR:
+%   A. J. Fassbender (NOAA-PMEL): andrea.j.fassbender@noaa.gov
+%
+% DATE: MAY 2, 2022
+
+
+%% Set Directory Paths -----------------------------------
+
+disp('set your paths')
+data_path = cd;
+fig_path  = cd;
+
+%% Calculate 1950s and 2090s Amplitudes -----------------------------------
+
+lat  = ncread([data_path '/Ensemble_Members/lat.nc'],  'lat');   
+lon  = ncread([data_path '/Ensemble_Members/lon.nc'],  'lon');  
+time = ncread([data_path '/Ensemble_Members/time.nc'],'time'); 
+
+yr      = str2num(datestr(time,'yyyy'));
+uyr     = unique(yr);
+ind_50s = find(yr < 1960);
+ind_90s = find(2090 <= yr & yr < 2100);
+
+vinfo = ncinfo([data_path '/Ensemble_Members/m1/ta.nc'],'ta');
+dim   = vinfo.Size; 
+
+vars = {'dic','ta','sst','wind','co2_totl','mld','co2_bp','co2_t','co2_t_nl','co2_bp_nl'};
+for i = 1:length(vars)
+    uvar = char(vars(i));
+    if strcmp(uvar,'mld') == 1        
+        eval([uvar '_max_50s = NaN(dim(1),dim(2),30);']);
+        eval([uvar '_max_90s = NaN(dim(1),dim(2),30);']);
+    else
+        eval(['A' uvar '_50s = NaN(dim(1),dim(2),30);']);
+        eval(['A' uvar '_90s = NaN(dim(1),dim(2),30);']);
+    end
+end
+
+st_0 = now;
+for ii = 1:30
+    st_1 = now;
+
+    fpath = [data_path '/Ensemble_Members/m' num2str(ii)];
+    disp(['Start ensemble member ' num2str(ii)])
+
+    % Calculate values for the two decades
+    for j = 1:2
+        disp(['Start processing decade ' num2str(j)])   
+        if j == 1
+            ind = ind_50s;
+            txt = '_50s';
+        else
+            ind = ind_90s;
+            txt = '_90s';
+        end 
+    
+        for i = 1:length(vars)
+            uvar = char(vars(i));
+            x    = ncread([fpath '/' uvar '.nc'],uvar,[1 1 ind(1)],[Inf Inf length(ind)]);
+            if strcmp(uvar,'mld') == 1
+                eval([uvar '_max' txt '(:,:,ii) = nanmean(squeeze(max(reshape(x,dim(1),dim(2),12,[]),[],3)),3);']);
+            else
+                x = eval(uvar);
+                eval(['A' uvar txt '(:,:,ii) = nanmean(squeeze(max(reshape(x,dim(1),dim(2),12,[]),[],3) - min(reshape(x,dim(1),dim(2),12,[]),[],3)),3);']);
+            end
+            clear x uvar
+        end
+    end
+    disp(['Complete ensemble member ' num2str(ii)])
+    minutes((now - st_1)*24*60)
+end
+disp('Total time:')
+minutes((now - st_0)*24*60)
+
+
+%% Chemical Leverage Impact on pCO2 BP and pCO2 T
+
+ilat       = -89.5:1:89.5;
+ilon       =  0.5:1:359.5;
+
+dco2_bp    = ncread(fpath,'Aco2_bp_90s')    - ncread(fpath,'Aco2_bp_50s');
+dco2_bp_nl = ncread(fpath,'Aco2_bp_nl_90s') - ncread(fpath,'Aco2_bp_nl_50s');
+dco2_bp_p  = 100 .* ((dco2_bp-dco2_bp_nl)./dco2_bp);
+area_weighted_mean(dco2_bp_p,ilat,ilon) % 45 ± 88 percent
+area_weighted_std(dco2_bp_p,ilat,ilon)
+
+dco2_t    = ncread(fpath,'Aco2_t_90s')    - ncread(fpath,'Aco2_t_50s');
+dco2_t_nl = ncread(fpath,'Aco2_t_nl_90s') - ncread(fpath,'Aco2_t_nl_50s');
+dco2_t_p  = 100 .* ((dco2_t-dco2_t_nl)./dco2_t);
+area_weighted_mean(dco2_t_p,ilat,ilon) % 101 ± 11 percent
+area_weighted_std(dco2_t_p,ilat,ilon) 
+
+
+% Save Results -----------------------------------
+
+fpath = [data_path '/Ensemble_Members'];
+
+vars = {'dic','ta','sst','wind','co2_totl','mld','co2_bp','co2_t','co2_t_nl','co2_bp_nl'};
+uni  = {'umol/kg','umol/kg','C','m/s','uatm','m','uatm','uatm','uatm','uatm'};
+
+for i = 1:length(vars)
+    if strcmp(vars(i),'mld') == 1
+        uvar = [char(vars(i)) '_max_50s'];
+        eval(['x = ' uvar ';']);
+        X = nanmean(x,3);
+        nccreate([fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'Dimensions',{'lon',dim(1),'lat',dim(2)})
+        ncwrite( [fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,X)
+        ncwriteatt([fpath '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'units',char(uni(i)))
+        ncwriteatt([fpath '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'long_name','1950s mean max annual MLD')
+
+        uvar = [char(vars(i)) '_max_90s'];
+        eval(['x = ' uvar ';']);
+        X = nanmean(x,3);
+        nccreate([fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'Dimensions',{'lon',dim(1),'lat',dim(2)})
+        ncwrite( [fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,X)
+        ncwriteatt([fpath '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'units',char(uni(i)))
+        ncwriteatt([fpath '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'long_name','2090s mean max annual MLD')
+    else
+        uvar = ['A' char(vars(i)) '_50s'];
+        eval(['x = ' uvar ';']);
+        X = nanmean(x,3);
+        nccreate([fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'Dimensions',{'lon',dim(1),'lat',dim(2)})
+        ncwrite( [fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,X)
+        ncwriteatt([fpath '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'units',char(uni(i)))
+        ncwriteatt([fpath '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'long_name','1950s mean seasonal amplitude')
+
+        uvar = ['A' char(vars(i)) '_90s'];
+        eval(['x = ' uvar ';']);
+        X = nanmean(x,3);
+        nccreate([fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'Dimensions',{'lon',dim(1),'lat',dim(2)})
+        ncwrite( [fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,X)
+        ncwriteatt([fpath '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'units',char(uni(i)))
+        ncwriteatt([fpath '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'],uvar,'long_name','2090s mean sesaaonal amplitude')
+    end
+end 
+
+
+%% Area-weighted changes globally -----------------------------------
+
+clearvars -except data_path fig_path
+
+vars = {'Adic','Ata','Asst','Awind','mld_max','Aco2_totl','Aco2_t','Aco2_t_nl','Aco2_bp','Aco2_bp_nl'};
+uni  = {'umol/kg','umol/kg','C','m/s','m','uatm','uatm','uatm','uatm','uatm'};
+
+fpath = [data_path '/Ensemble_Members'];
+area  = reshape(transpose(getArea(-89.5:1:89.5,.5:359.5)),[],1);
+
+for i = 1:length(vars)
+    X_50 = ncread([fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'], [char(vars(i)) '_50s']);  
+    X_90 = ncread([fpath   '/Ensemble_Mean_AmplitudeChanges_1950_2090.nc'], [char(vars(i)) '_90s']);   
+    
+    delta = X_90-X_50;
+    wmean(i,1)  = area_weighted_mean(delta,ilat,ilon);
+    wstdev(i,1) = area_weighted_std( delta,ilat,ilon);
+
+    delta_p = 100 .* delta./X_50;
+    wmean_p(i,1)  = area_weighted_mean(delta_p,ilat,ilon);
+    wstdev_p(i,1) = area_weighted_std( delta_p,ilat,ilon);
+end 
+
+t0 = cell2table(vars(:),'VariableNames',{'1950s-2090s Change'});
+t1 = cell2table(uni(:),'VariableNames',{'Units'});
+t2 = array2table([wmean wstdev wmean_p wstdev_p],'VariableNames',{'Weighted Mean' 'Weighted StDev' 'Weighted Mean (%)' 'Weighted StDev (%)'});
+t  = [t0 t2 t1]
+save([fig_path '/Ensemble_Mean_AmplitudeChanges_1950_2090.mat'], 't')
+
+%% ----- Clean up
+
+%clear;close all;clc
---
+++ Figure_5.m
@@ -0,0 +1,203 @@
+%% Figure 5
+%
+% DESCRIPTION:
+%   Cumulative flux result maps for different chemical leverage configurations.
+%
+% USER INPUT:
+%   data_path: directory containing reprocessed .nc files
+%   fig_path:  directory where figure will be saved
+%
+% FUNCTIONS CALLED:
+%   m_map toolbox: https://www.eoas.ubc.ca/~rich/map.html
+%   tight_subplot: https://www.mathworks.com/matlabcentral/fileexchange/27991-tight_subplot-nh-nw-gap-marg_h-marg_w
+%
+% AUTHOR:
+%   A. J. Fassbender (NOAA-PMEL): andrea.j.fassbender@noaa.gov
+%
+% DATE: MAY 2, 2022
+
+
+%% Set Directory Paths -----------------------------------
+
+disp('set your paths')
+data_path = cd;
+fig_path  = cd;
+
+
+%%  Global Figures -----------------------------------
+
+area  = getArea(-89.5:1:89.5,.5:359.5);
+lat   = ncread([data_path '/Ensemble_Members/lat.nc'], 'lat');   
+lon   = ncread([data_path '/Ensemble_Members/lon.nc'], 'lon'); 
+
+fpath = [data_path '/Ensemble_Members'];
+
+% Flux Differences
+flux_l    = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'uptake_l'); 
+flux_nl   = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'uptake_nl');
+flux_tl   = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'uptake_tl');
+flux_bpl  = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'uptake_bpl');
+d_l_nl    = flux_l   - flux_nl; 
+d_tl_nl   = flux_tl  - flux_nl; 
+d_bpl_nl  = flux_bpl - flux_nl;
+
+var   = {'flux_l', 'd_l_nl', 'd_tl_nl', 'd_bpl_nl'};
+crng1 = [-400 -100 -100 -100];
+crng2 = [ 400  100  100  100];
+cint  = [  80   20   20   20];
+tvar  = {'Cumulative Flux (',...
+         'Impact of Chem. Lev. (',...
+         'Impact of T Lev. (',...
+         'Impact of BP Lev. ('};
+
+% Map projection limits
+Slongs =[-100.5 46.5;-76.5 25.5; 25.5 142.5;46.5 97.5;142.5 295;97.5 295];
+Slats  = [0 90;-90 0;-90 0;0 90;-90 0;0 90];
+
+x       = find(lon > 180);
+llon    = lon;
+llon(x) = lon(x)-360;
+LON2    = [transpose(llon) .5];
+
+figure(1);clf
+set(gcf,'Units','Inches');
+set(gcf,'position',[2 12 6.5 3.8])
+set(gcf,'Resize','off')
+clear ha;ha = tight_subplot(2,2,[0 0],[0 .045],[0 0]);
+
+% Average cumulative uptake across models
+tflux_l     = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_l'); 
+tflux_nl    = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_nl');
+tflux_tl    = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_tl');
+tflux_bpl   = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_bpl');
+CU = [tflux_l; tflux_l - tflux_nl; tflux_l - tflux_bpl; tflux_l - tflux_tl];
+
+for n = 1:length(var)
+    clear xvar 
+    eval(['xvar = ' char(var(n)) ';']);
+    xvar = transpose(xvar);
+    clear X;X = [xvar xvar(:,1)];
+    
+    axes(ha(n));hold on    
+    for l=1:6
+        m_proj('mollweide','long',Slongs(l,:),'lat',Slats(l,:));
+        m_grid('fontsize',6,'xticklabels',[],'xtick',[],...
+            'ytick',[],'yticklabels',[],'linest','-','color',[.4 .4 .4]);
+        if l<3
+            h=m_pcolor(LON2,lat,X);hold on;%shading interp
+            set(h, 'EdgeColor', 'none');
+            m_coast('patch',[.5 .5 .5]);
+            m_grid('tickdir','in','linestyle','none','backcolor',[.75 .75 .75],'xticklabels',[],'yticklabels',[]);
+        else
+            h=m_pcolor(lon,lat,xvar);hold on;%shading interp
+            set(h, 'EdgeColor', 'none');
+            m_coast('patch',[.5 .5 .5]);
+            m_grid('tickdir','in','linestyle','none','backcolor',[.75 .75 .75],'xticklabels',[],'yticklabels',[]);
+        end
+    end
+    
+    set(gca,'xlimmode','auto','ylimmode','auto');
+    t1 = [char(tvar(n))  num2str(round(CU(n))) ' PgC)'];
+    T= title({t1,'(mol C m^{-2})'},'fontweight','normal');set(T,'position',[1.0000 1.0098+.08 -5.0000e+15])
+    h = colorbar;
+
+    bn = crng1(n):cint(n):crng2(n);caxis([min(bn) max(bn)]);
+    cc = cmocean('balance',length(bn));colormap(gca,cc);
+
+    if n == 1
+        set(h,'position',[0.072 0.58 0.0175 0.13])
+    elseif n == 2
+        set(h,'position',[0.571 0.58 0.0175 0.13])
+    elseif n == 3
+        set(h,'position',[0.072 0.1 0.0175 0.13])
+    elseif n == 4
+        set(h,'position',[0.571 0.1 0.0175 0.13])
+    end
+end
+
+
+%% Subplot letters -----------------------------------
+
+let = {'(a)','(b)','(c)','(d)','(e)','(f)'};
+for n = 1:4
+    axes(ha(n));hold on    
+    yylim=get(gca,'YLim');xxlim=get(gca,'XLim');
+    clear xr; xr = diff(xxlim);
+    clear yr; yr = diff(yylim);
+    text(xxlim(1)+0.05*xr,yylim(2)+0.06.*yr,let(n),'VerticalAlignment','bottom','HorizontalAlignment','left','fontsize',8,'fontweight','normal','fontname','Arial')
+end
+
+
+%% Save -----------------------------------
+
+fig=gcf;
+set(findall(fig,'-property','Fontname'),'fontname','Times','fontsize',9,'fontweight','normal');
+exportgraphics(fig,[fig_path '/Figure_5.pdf'],'Resolution',300)
+savefig([fig_path '/Figure_5'])
+
+
+%%  Table -----------------------------------
+
+% Average cumulative uptake across models
+
+flux_l     = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_l'); 
+flux_nl    = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_nl');
+flux_tl    = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_tl');
+flux_bpl   = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_bpl');
+flux_model = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_model'); 
+x1 = [flux_l;flux_nl;flux_bpl;flux_tl;flux_model] % Pg C
+
+% StDev of cumulative uptake (PgC) across runs;
+
+flux_l_sd     = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_l_sd'); 
+flux_nl_sd    = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_nl_sd');
+flux_tl_sd    = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_tl_sd');
+flux_bpl_sd   = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_bpl_sd');
+flux_model_sd = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'tot_uptake_model_sd'); 
+x2 = [flux_l_sd;flux_nl_sd;flux_bpl_sd;flux_tl_sd;flux_model_sd] % Pg C
+
+vars    = {'Chem Lev','No Lev','BP Lev','T Lev','Model Output'};
+delta   = x1 - x1(2);
+delta_p = 100.*(x1 - x1(2))./x1(2);
+
+t0 = cell2table(vars(:),'VariableNames',{'Cumulative Global Flux'});
+t1 = array2table([x1 x2 delta delta_p],'VariableNames',{'EM Pg C' 'EM Pg C (StDev)' 'Uptake Diff' '% Diff'});
+t  = [t0 t1]
+
+% Differences relative to no-leverage
+%  8.1% more uptake in reconstruction w/ leverage
+% 18.4% less uptake in reconstruction w/ bp leverage only
+% 10.2% more uptake in reconstruction w/ t leverage only
+
+% Difference between reconstruction and model output
+x1(end) - x1(1);
+100.*(x1(end)- x1(1))./x1(1)
+%  1.1% less uptake in model output relative to reconstruction w/ leverage
+
+% Error Propagation for Leverage vs. No Leverage
+v = (x2(1)^2 + x2(2)^2)^.5 
+% --> 1.7284 PgC; uncertainty in the PgC difference (37 ± 2 Pg C)
+
+val = 100 .* (x1(1) - x1(2))/ x1(2)
+(val) .* ((v./(x1(1) - x1(2)))^2 + (x2(2)./x1(2))^2 ) ^.5
+% --> 0.3801 % uncertainty in the percentage difference (8.1 ± 0.4 %)
+
+
+%% North Pacific Percentage -----------------------------------
+
+% regional boundaries [lat_min lat_max lon_min lon_mat];
+r_ind = [ 30 42 140 240];
+
+x1 = find(r_ind(1) <= lat & lat <= r_ind(2));
+x2 = find(r_ind(3) <= lon & lon <= r_ind(4));
+
+flux_l     = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'uptake_l'); 
+flux_nl    = ncread([fpath '/EnsembleMean_C_Uptake.nc'],'uptake_nl');
+f   = squeeze(nansum(nansum(flux_l(x2,x1,:)  .* transpose(area(x1,x2)))) .* 12 ./ (10^15));% Pg C/mo
+fnl = squeeze(nansum(nansum(flux_nl(x2,x1,:) .* transpose(area(x1,x2)))) .* 12 ./ (10^15));% Pg C/mo
+
+(f-fnl)./f % 14 percent
+
+%% Clean up -----------------------------------
+
+clear;close all;clc
