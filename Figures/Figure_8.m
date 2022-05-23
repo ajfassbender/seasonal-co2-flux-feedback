2540ed1f17966a73d0e30f543a32dbfbde53006e
Andrea Fassbender <55896000+ajfassbender@users.noreply.github.com>
2022-05-23 13:53:54 -0700

    Update Figure_8.m

--- Figures/Figure_8.m
+++ Figures/Figure_8.m
@@ -10,6 +10,7 @@
 % FUNCTIONS CALLED:
 %   subtightplot: https://www.mathworks.com/matlabcentral/fileexchange/39664-subtightplot
 %   area_weighted_mean: computes area weighted mean
+%   rgb: https://www.mathworks.com/matlabcentral/fileexchange/24497-rgb-triple-of-color-name-version-2/
 % 
 % AUTHOR:
 %   A. J. Fassbender (NOAA-PMEL): andrea.j.fassbender@noaa.gov
